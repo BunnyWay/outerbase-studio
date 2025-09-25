feac84260acd17ed6d0a766192c087a1acab9aaf
Saso Jermancic <saso@bunny.net>
2025-09-25 22:37:33 +0200

    Put back sidebar and remove popup on outerbase, adapt sizebar width

--- src/components/gui/database-gui.tsx
+++ src/components/gui/database-gui.tsx
@@ -27,7 +27,7 @@ import SavedDocTab from "./sidebar/saved-doc-tab";
 export default function DatabaseGui() {
   const DEFAULT_WIDTH = 250;
 
-  const [defaultWidthPercentage, setDefaultWidthPercentage] = useState(15);
+  const [defaultWidthPercentage, setDefaultWidthPercentage] = useState(20);
 
   useEffect(() => {
     setDefaultWidthPercentage((DEFAULT_WIDTH / window.innerWidth) * 100);
--- src/components/gui/sidebar-tab.tsx
+++ src/components/gui/sidebar-tab.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
 import { useSearchParams } from "next/navigation";
 import { ReactElement, useState } from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export interface SidebarTabItem {
   key: string;
@@ -33,21 +34,21 @@ export default function SidebarTab({ tabs }: Readonly<SidebarTabProps>) {
 
   return (
     <div className={cn("flex h-full bn-bg-main")}>
-      {/* <div className={cn("shrink-0")}>
+      <div className={cn("shrink-0")}>
         <div className="flex h-full flex-col gap-4 border-r border-neutral-200 p-3 dark:border-neutral-700">
-          <DropdownMenu modal={false}>
-            <DropdownMenuTrigger>
+          {/* <DropdownMenu modal={false}>
+            <DropdownMenuTrigger> */}
               <div className="mb-2 ml-1 flex h-8 w-8 items-center justify-center">
                 <svg
                   fill="currentColor"
                   viewBox="75 75 350 350"
-                  className="h-10 w-10 cursor-pointer text-black dark:text-white"
+                  className="h-10 w-10 text-black dark:text-white"
                 >
                   <path d="M249.51,146.58c-58.7,0-106.45,49.37-106.45,110.04c0,60.68,47.76,110.04,106.45,110.04 c58.7,0,106.46-49.37,106.46-110.04C355.97,195.95,308.21,146.58,249.51,146.58z M289.08,332.41l-0.02,0.04l-0.51,0.65 c-5.55,7.06-12.37,9.35-17.11,10.02c-1.23,0.17-2.5,0.26-3.78,0.26c-12.94,0-25.96-9.09-37.67-26.29 c-9.56-14.05-17.84-32.77-23.32-52.71c-9.78-35.61-8.67-68.08,2.83-82.74c5.56-7.07,12.37-9.35,17.11-10.02 c13.46-1.88,27.16,6.2,39.64,23.41c10.29,14.19,19.22,33.83,25.12,55.32C301,285.35,300.08,317.46,289.08,332.41z"></path>
                 </svg>
               </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent side="right" align="start">
+            {/* </DropdownMenuTrigger> */}
+            {/* <DropdownMenuContent side="right" align="start">
               <div
                 className="m-1 mb-2 flex h-[120px] w-[250px] flex-col justify-end rounded"
                 style={{
@@ -98,8 +99,8 @@ export default function SidebarTab({ tabs }: Readonly<SidebarTabProps>) {
                   About us
                 </Link>
               </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            </DropdownMenuContent> */}
+          {/* </DropdownMenu> */}
 
           {tabs.map(({ key, name, icon, onClick }, idx) => {
             return (
@@ -136,7 +137,7 @@ export default function SidebarTab({ tabs }: Readonly<SidebarTabProps>) {
             );
           })}
         </div>
-      </div> */}
+      </div>
 
       <div className="relative flex h-full grow overflow-hidden">
         {tabs
